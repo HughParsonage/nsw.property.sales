6299b7c60c360a8ab4138b47a237299dbafb8c37
HughParsonage <hugh.parsonage@gmail.com>
2017-10-10 14:14:47 +1100

    Add documentation; set travis

---
+++ R/data-PropertySales20012017.R
@@ -0,0 +1,4 @@
+#' @title Property Sales 2001-20017
+#' @description All property sales recorded by the NSW Valuer-General
+
+"PropertySales20012017"
---
+++ R/package-nswpropertysales.R
@@ -0,0 +1,9 @@
+#' @title NSW Property Sales information
+#' @name nsw.property.sales
+#' @author Hugh Parsonage
+#' @description Lazily loads data from the NSW Valuer-General
+#'
+#'
+#' @import data.table
+
+NULL
