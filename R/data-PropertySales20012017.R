73ace6f94385616ad2276eb369cc807c76023f82
HughParsonage <hugh.parsonage@gmail.com>
2018-08-26 16:41:52 +1000

    Update data

--- R/data-PropertySales20012017.R
+++ R/data-PropertySales20012017.R
@@ -1,4 +1,5 @@
 #' @title Property Sales 2001-2017
+#' @name PropertySales
 #' @description All property sales recorded by the NSW Valuer-General since 2001.
 #' Field names are provided 'as-is', except some dates that appeared to be
 #' typos were corrected. Zone codes have been decoded, but \code{District_code}
@@ -10,5 +11,10 @@
 #' an integer that may change as this dataset is updated. Note that little is lost by using
 #' \code{DEALING_ID} as opposed to \code{Dealing_no}, unless you know the \code{Dealing_no},
 #' as it is simply a unique identifier of the dealing.
+NULL
 
+#' @rdname PropertySales
 "PropertySales20012017"
+
+#' @rdname PropertySales
+"PropertySales19902000"
